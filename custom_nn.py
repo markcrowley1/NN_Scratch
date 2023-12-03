5c473df2def518819bad80008de9a0700061537a
markcrowley1 <markcrowley1999@gmail.com>
2023-12-03 19:30:36 +0000

    making progress towards implementing backpropagation algorithm with batch training

--- custom_nn.py
+++ custom_nn.py
@@ -5,50 +5,60 @@ Description:
 
 import numpy as np
 
-""" Defining Numpy Vector Functions"""
+""" Defining Numpy Vector Functions """
 def sigmoid(x):
-    """ Sigmoid/Logistic Function"""
+    """ Sigmoid/Logistic Function """
     return 1 / (1 + np.exp(x))
 
 def sigmoid_derivative(x):
-    """ Derivative of Sigmoid Function"""
+    """ Derivative of Sigmoid Function """
     return sigmoid(x) * (1 - sigmoid(x))
 
 def softmax(x: np.ndarray):
-    """ Softmax function for output layer """
+    """ Softmax function for Output Vectors """
     # # Values shifted to reduce chance of generating NaN values
     # x = x - np.max(x)
     exp_x = np.exp(x)
-    return exp_x / np.sum(exp_x)
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
 def softmax_derivative(S: np.ndarray):
     """ Computes Gradient of Softmax Function """
     # Input S should be the vector output of the Softmax function
-    S_diag = np.diag(S)
-    S_vector = S.reshape(S.shape[0], 1)
-    S_matrix = np.tile(S_vector, S.shape[0])
+    gradients = []
+    for row in S:
+        S_diag = np.diag(row)
+        S_vector = row.reshape(row.shape[0], 1)
+        S_matrix = np.tile(S_vector, row.shape[0])
 
-    # Compute jacobian derivative with matrix maths
-    gradient = S_diag - (S_matrix * np.transpose(S_matrix))
-    return gradient
+        # Compute jacobian derivative with matrix maths
+        gradient = S_diag - (S_matrix * np.transpose(S_matrix))
+        gradients.append(gradient)
+    return np.stack(gradients, axis=0)
 
-def categorical_cross_entropy(predicted: np.ndarray, target: np.ndarray):
-    """ Categorical Cross Entropy Cost Function"""
+def categorical_cross_entropy(predictions: np.ndarray, targets: np.ndarray):
+    """ Categorical Cross Entropy Cost Function for a Batch of Samples """
     losses = []
-    for t, p in zip(target, predicted):
+    for t, p in zip(targets, predictions):
         loss = -np.sum(t * np.log(p))
         losses.append(loss)
-    return losses
+    return np.sum(losses)
+
+def cce_derivative(predictions: np.ndarray, targets: np.ndarray):
+    """ Derivative of Categorical Cross Entropy cost function """
+    # Gradient is simply the difference between the
+    # predicted probability values and the true probability values.
+    return predictions - targets
 
 """ Defining Classes for MLP and Individual Layers """
 class MLP:
     BATCH_SIZE = 32
+    LEARNING_RATE = 0.1
 
     def __init__(self, num_inputs: int, num_outputs: int, hidden_layers_sizes: list):
         # Create Layers
         input_layer = Layer(num_inputs)
-        output_layer = Layer(num_outputs)
-        hidden_layers = [Layer(layer_size) for layer_size in hidden_layers_sizes]
+        output_layer = Layer(num_outputs, "Softmax")
+        hidden_layers = [Layer(layer_size, "Sigmoid") for layer_size in hidden_layers_sizes]
         
         # Link Layers to form Sequential Graph
         self.graph = [input_layer] + hidden_layers + [output_layer]
@@ -70,8 +80,50 @@ class MLP:
             layer: Layer = self.graph[i]
             layer.initialise_biases()
 
-    def train(self, x_data, y_data, epochs: int = 3):
-        pass
+    def train(self, x_data: np.ndarray, y_data: np.ndarray, epochs: int = 3):
+        """ Train the network """
+        sample_count = x_data.shape[0]
+        print(f"Training network on {sample_count} examples for {epochs} epochs.")
+        print(f"Batch Size: {self.BATCH_SIZE}")
+        print(f"Learning Rate: {self.LEARNING_RATE}")
+        for epoch in range(epochs):
+            # Slice data into batches for training and keep going until all data is used
+            batch_start_index = 0
+            batch_end_index = self.BATCH_SIZE
+            while batch_end_index < sample_count:
+                # Create slices
+                batch_x = x_data[batch_start_index:batch_end_index]
+                batch_y = y_data[batch_start_index:batch_end_index]
+                batch_start_index = batch_end_index
+                batch_end_index += self.BATCH_SIZE
+                # Feed forward to get predictions
+                batch_y_hat = self.feed_forward(batch_x)
+                cce_grad = cce_derivative(batch_y_hat, batch_y)
+                prev_error = cce_grad
+                print(cce_grad.shape)
+                # Iterate backwards through graph and find gradients
+                for i in reversed(range(1, len(self.graph))):
+                    layer: Layer = self.graph[i]
+                    activation_error = layer.activation_function_derivative(layer.pre_activation_values)
+                    print(activation_error.shape)
+                    # error_chain = np.sum()
+                    # layer.weight_grad = error_chain.dot()
+
+                # Update weights and biases
+                break
+            # Use categorical cross entropy function to get loss
+            loss = categorical_cross_entropy(batch_y_hat, batch_y)
+            print(f"Epoch {epoch+1} completed. Loss: {loss}")
+
+    def feed_forward(self, x_data: np.ndarray):
+        """ Run input through network and return prediction """
+        prev_output = x_data.reshape((self.BATCH_SIZE, -1))
+        for i in range(1, len(self.graph)):
+            layer: Layer = self.graph[i]
+            layer.pre_activation_values = prev_output.dot(layer.weights) + layer.biases
+            layer.activation_values = layer.activation_function(layer.pre_activation_values)
+            prev_output = layer.activation_values
+        return self.graph[-1].activation_values
 
     def print_graph(self):
         for i, layer in enumerate(self.graph):
@@ -88,18 +140,31 @@ class MLP:
         print("---------------------------------------------")
 
 class Layer:
-    def __init__(self, layer_size: int):
+    def __init__(self, layer_size: int, activation_function=None):
         # Record number of neurons in layer
         self.layer_size = layer_size
         # Create activations which stores neuron outputs
         # Create bias and weight attributes to initialise later
         self.activation_values = None
+        self.pre_activation_values = None
         self.biases = None
         self.weights = None
+        self.bias_grad = None
+        self.weight_grad = None
+        # Set activation function
+        if activation_function == "Sigmoid":
+            self.activation_function = sigmoid
+            self.activation_function_derivative = sigmoid_derivative
+        elif activation_function == "Softmax":
+            self.activation_function = softmax
+            self.activation_function_derivative = softmax_derivative
+        else:
+            self.activation_function = None
+            self.activation_function_derivative = None
 
     def initialise_weights(self, prev_layer_size: int):
         """ Initialise one weight value for each neuron connection"""
-        self.weights = np.random.rand(self.layer_size, prev_layer_size)
+        self.weights = np.random.rand(prev_layer_size, self.layer_size)
     
     def initialise_biases(self):
         """ Initialise bias value for each neuron in layer"""
--- run_custom_mnist.py
+++ run_custom_mnist.py
@@ -1,28 +1,59 @@
 
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from custom_nn import *
 
+def view_data(x):
+    print(x.shape)
+    plt.imshow(x, cmap = plt.cm.binary)
+    plt.show()
+    return
+
 def main():
+    # Load in MNIST data
+    dataset = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = dataset.load_data()
+
+    # Normalise Inputs for Training and Test Data
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
+    image, label = x_train[0], y_train[0]
+
+    # Convert labels to one hot encoding
+    y_train_oh = np.zeros((y_train.size, y_train.max() + 1))
+    y_train_oh[np.arange(y_train.size), y_train] = 1
+
     # Create network and print graph
     model = MLP(784, 10, [128,128])
-    model.print_graph()
+    # predictions = model.feed_forward(image)
+    # print(np.sum(predictions), predictions)
+    model.train(x_train, y_train_oh, epochs=1)
+    # view_data(image)
 
-    # Test out activation functions
-    # Generate random values to represent NN output
-    pseudo_output = np.random.rand(10)
-    print(pseudo_output)
-    # Check the output of the Softmax function
-    S = softmax(pseudo_output)
-    print(np.sum(S), S)
-    # Find the gradient of the softmax
-    dS_dx = softmax_derivative(S)
+    # # Test out activation functions
+    # # Generate random values to represent NN output
+    # pseudo_output = np.random.rand(5, 10)
+    # pseudo_target = np.tile([1,0,0,0,0,0,0,0,0,0], (5,1))
+    # print(f"Predictions:\n {pseudo_output}\n")
+    # print(f"Targets:\n {pseudo_target}\n")
+    # # Check the output of the Softmax function
+    # S = softmax(pseudo_output)
+    # print(f"{np.sum(S)}, {S}")
+    # # Test out categorical cross entropy loss function
+    # loss = categorical_cross_entropy(S, pseudo_target)
+    # print(f"Loss:\n {loss}\n")
+    # dL_dz = cce_derivative(S, pseudo_target)
+    # print(f"Derivative of Cost Function:\n {dL_dz}\n")
+    # # # Find the gradient of the softmax
+    # # dS_dx = softmax_derivative(S)
     
-    # Test out sigmoid function and its derivative
-    A = sigmoid(pseudo_output)
-    print(pseudo_output)
-    dA_dx = sigmoid_derivative(A)
-    print(dA_dx)
+    # # Test out sigmoid function and its derivative
+    # A = sigmoid(pseudo_output)
+    # print(pseudo_output)
+    # dA_dx = sigmoid_derivative(A)
+    # print(dA_dx)
 
 
 if __name__ == "__main__":
